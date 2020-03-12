d21086213d60fd5cce1f4d2c41464aafc97c6c68
Kunal Kumar <kunalkr789@gmail.com>
2020-03-12 12:55:38 +0530

    converting to ajax

--- models/user.js
+++ models/user.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 
+const multer = require('multer');
+const path = require('path');
+const AVATAR_PATH = require('/uploads/users/avatars');
+
 const userSchema = new mongoose.Schema({
     email:{
         type: String,
